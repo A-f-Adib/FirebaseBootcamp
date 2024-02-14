7465b1556d0f843cf89406ed9d7c46b0dd9fd9e4
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-14 12:18:24 +0600

    1st commit

---
+++ FirebaseBootcamp/FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseBootcampApp.swift
+//  FirebaseBootcamp
+//
+//  Created by A.f. Adib on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
